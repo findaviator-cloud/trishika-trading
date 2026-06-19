f5cd75d72a6108fd331039b50f7c448e6264b3f6
Himanshu Chaubey <ppchaubey98@gmail.com>
2026-06-19 06:20:17 +0000

    fix: TATAMOTORS Yahoo Finance symbol .BO -> .NS

--- src/angel/index.js
+++ src/angel/index.js
@@ -137,7 +137,7 @@ export async function loadFnoHistory(engines, log) {
     "KOTAKBANK":  "KOTAKBANK.NS",
     "LT":         "LT.NS",
     "ONGC":       "ONGC.NS",
-    "TATAMOTORS": "TATAMOTORS.BO",
+    "TATAMOTORS": "TATAMOTORS.NS",
     "BAJFINANCE": "BAJFINANCE.NS",
     "MARUTI":     "MARUTI.NS",
     "ADANIENT":   "ADANIENT.NS",
